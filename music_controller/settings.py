3f1ded89e2da7963e0d733a34465354e405687d2
Khagesh000 <128826992+Khagesh000@users.noreply.github.com>
2023-12-15 09:53:46 +0530

    commit

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'static'),
+]
 
 
 # Default primary key field type
